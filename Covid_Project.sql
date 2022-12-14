7b96f14b4a65d2469ffb48e0d62c2a95afa45c45
Sushmita <68901639+sushmitapr@users.noreply.github.com>
2022-12-14 21:24:38 +0530

    Update Covid_Project.sql

--- Covid_Project.sql
+++ Covid_Project.sql
@@ -1,4 +1,3 @@
-use proj
 select * from CovidDeaths$
 order by 3,4
 
@@ -141,5 +140,42 @@ where cd.continent is not null
 
 select * from percent_populationvaccinated
 
+
+
+-- Queries used for visulaization
+
+
+--1
+
+Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+From CovidDeaths$
+where continent is not null 
+order by 1,2
+
+--2
+
+Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
+From CovidDeaths$
+Where continent is null 
+and location not in ('World', 'European Union', 'International')
+Group by location
+order by TotalDeathCount desc
+
+
+-- 3
+
+Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From CovidDeaths$
+Group by Location, Population
+order by PercentPopulationInfected desc
+
+
+-- 4
+
+
+Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From CovidDeaths$
+Group by Location, Population, date
+order by PercentPopulationInfected desc
 
  
